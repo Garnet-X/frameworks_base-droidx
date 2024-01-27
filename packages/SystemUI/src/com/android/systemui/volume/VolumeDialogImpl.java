d0a6836cb660086e4044e1a2ea74a1c37bab9835
danielml <daniel@danielml.dev>
2024-01-27 19:07:00 +0100

    SystemUI: VolumeDialogImpl: Do not override the layout direction
    * When the layout direction changes, mark as configuration changed
      so that the dialog re-initializes, thus setting the correct direction
      depending on the user's volume dialog position settings
    
    Change-Id: I0b9f15972d0416647ba045b7776bed8e6250d57f
    Signed-off-by: danielml <daniel@danielml.dev>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -2436,7 +2436,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
 
         @Override
         public void onLayoutDirectionChanged(int layoutDirection) {
-            mDialogView.setLayoutDirection(layoutDirection);
+            onConfigurationChanged();
         }
 
         @Override
